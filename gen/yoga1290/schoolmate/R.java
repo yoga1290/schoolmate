a64322a047a98ec5368694cb93fcff9c2abdb0e0
Youssef Gamil <yoga1290@gmail.com>
2013-06-29 01:32:30 +0200

    bin

--- gen/yoga1290/schoolmate/R.java
+++ gen/yoga1290/schoolmate/R.java
@@ -8,7 +8,22 @@
 package yoga1290.schoolmate;
 
 public final class R {
+    public static final class array {
+        public static final int pref_example_list_titles=0x7f070000;
+        public static final int pref_example_list_values=0x7f070001;
+        public static final int pref_sync_frequency_titles=0x7f070002;
+        public static final int pref_sync_frequency_values=0x7f070003;
+    }
     public static final class attr {
+    }
+    public static final class dimen {
+        /** 
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int checkin=0x7f020000;
@@ -21,69 +36,106 @@ public final class R {
         public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int audioPeer=0x7f07001e;
-        public static final int audioPeerOk=0x7f07001f;
-        public static final int button_class_details_fqsr=0x7f07000f;
-        public static final int button_class_stream=0x7f070011;
-        public static final int button_connect=0x7f070006;
-        public static final int button_connect4sqr=0x7f070004;
-        public static final int button_connect_google=0x7f070007;
-        public static final int button_connectfb=0x7f070005;
-        public static final int button_post_row_fb=0x7f070017;
-        public static final int button_post_row_tw=0x7f070016;
-        public static final int checkin=0x7f070009;
-        public static final int editText_class_steam=0x7f070010;
-        public static final int editText_pin=0x7f070002;
-        public static final int et_connect=0x7f070003;
-        public static final int imageView1=0x7f070008;
-        public static final int imageView_class_details=0x7f07000e;
-        public static final int imageView_classes_row=0x7f070014;
-        public static final int imageView_post_row=0x7f070018;
-        public static final int imageview_profile_gpa=0x7f07001d;
-        public static final int imageview_profile_picture=0x7f07001a;
-        public static final int linearLayout_class_agenda=0x7f07000b;
-        public static final int linearLayout_class_details=0x7f07000d;
-        public static final int linearLayout_class_stream=0x7f070012;
-        public static final int linearLayout_classes=0x7f070013;
-        public static final int linearLayout_profile_info=0x7f07001c;
-        public static final int menu_settings=0x7f070021;
-        public static final int pager=0x7f070000;
-        public static final int pager_title_strip=0x7f070001;
-        public static final int scrollView1=0x7f07000c;
-        public static final int stopbutton=0x7f070020;
-        public static final int textView1=0x7f07000a;
-        public static final int textView_classes_row=0x7f070015;
-        public static final int textView_post_row=0x7f070019;
-        public static final int textview_profile_name=0x7f07001b;
+        public static final int action_settings=0x7f0a0025;
+        public static final int audioPeer=0x7f0a0021;
+        public static final int audioPeerOk=0x7f0a0022;
+        public static final int button_class_details_fqsr=0x7f0a0016;
+        public static final int button_class_stream=0x7f0a0018;
+        public static final int button_connect=0x7f0a0006;
+        public static final int button_connect4sqr=0x7f0a0004;
+        public static final int button_connect_google=0x7f0a0007;
+        public static final int button_connectfb=0x7f0a0005;
+        public static final int button_post_row_fb=0x7f0a001e;
+        public static final int button_post_row_tw=0x7f0a001d;
+        public static final int checkin=0x7f0a0010;
+        public static final int editText_class_steam=0x7f0a0017;
+        public static final int editText_pin=0x7f0a0002;
+        public static final int et_connect=0x7f0a0003;
+        public static final int imageView1=0x7f0a000f;
+        public static final int imageView_activity_profile_row_class=0x7f0a000d;
+        public static final int imageView_class_details=0x7f0a0015;
+        public static final int imageView_classes_row=0x7f0a001b;
+        public static final int imageView_post_row=0x7f0a001f;
+        public static final int imageview_profile_gpa=0x7f0a000a;
+        public static final int imageview_profile_picture=0x7f0a0008;
+        public static final int linearLayout_class_agenda=0x7f0a0012;
+        public static final int linearLayout_class_details=0x7f0a0014;
+        public static final int linearLayout_class_stream=0x7f0a0019;
+        public static final int linearLayout_classes=0x7f0a001a;
+        public static final int linearLayout_profile_classes=0x7f0a000b;
+        public static final int linearLayout_profile_info=0x7f0a000c;
+        public static final int menu_settings=0x7f0a0024;
+        public static final int pager=0x7f0a0000;
+        public static final int pager_title_strip=0x7f0a0001;
+        public static final int scrollView1=0x7f0a0013;
+        public static final int stopbutton=0x7f0a0023;
+        public static final int textView1=0x7f0a0011;
+        public static final int textView_activity_profile_row_class=0x7f0a000e;
+        public static final int textView_classes_row=0x7f0a001c;
+        public static final int textView_post_row=0x7f0a0020;
+        public static final int textview_profile_name=0x7f0a0009;
     }
     public static final class layout {
         public static final int activity_class=0x7f030000;
         public static final int activity_connect=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int view1=0x7f030003;
-        public static final int view4sqr=0x7f030004;
-        public static final int view_class_agenda=0x7f030005;
-        public static final int view_class_details=0x7f030006;
-        public static final int view_class_stream=0x7f030007;
-        public static final int view_classes=0x7f030008;
-        public static final int view_classes_row=0x7f030009;
-        public static final int view_post_row=0x7f03000a;
-        public static final int view_profile=0x7f03000b;
-        public static final int view_record=0x7f03000c;
+        public static final int activity_profile=0x7f030003;
+        public static final int activity_profile_row_class=0x7f030004;
+        public static final int view1=0x7f030005;
+        public static final int view4sqr=0x7f030006;
+        public static final int view_class_agenda=0x7f030007;
+        public static final int view_class_details=0x7f030008;
+        public static final int view_class_stream=0x7f030009;
+        public static final int view_classes=0x7f03000a;
+        public static final int view_classes_row=0x7f03000b;
+        public static final int view_post_row=0x7f03000c;
+        public static final int view_profile=0x7f03000d;
+        public static final int view_record=0x7f03000e;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f090000;
+        public static final int profile=0x7f090001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int button=0x7f040005;
-        public static final int button1=0x7f040004;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int stopbutton=0x7f040006;
-        public static final int title_activity_main=0x7f040003;
+        public static final int action_settings=0x7f060008;
+        public static final int app_name=0x7f060000;
+        public static final int button=0x7f060005;
+        public static final int button1=0x7f060004;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int pref_default_display_name=0x7f06000e;
+        public static final int pref_description_social_recommendations=0x7f06000c;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f060010;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f06000a;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f060013;
+        public static final int pref_ringtone_silent=0x7f060016;
+        public static final int pref_title_add_friends_to_messages=0x7f06000f;
+        public static final int pref_title_display_name=0x7f06000d;
+        public static final int pref_title_new_message_notifications=0x7f060014;
+        public static final int pref_title_ringtone=0x7f060015;
+        public static final int pref_title_social_recommendations=0x7f06000b;
+        public static final int pref_title_sync_frequency=0x7f060011;
+        public static final int pref_title_system_sync_settings=0x7f060012;
+        public static final int pref_title_vibrate=0x7f060017;
+        public static final int stopbutton=0x7f060006;
+        public static final int title_activity_main=0x7f060003;
+        public static final int title_activity_profile=0x7f060007;
+        public static final int title_activity_settings=0x7f060009;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f080000;
+    }
+    public static final class xml {
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
     }
 }
