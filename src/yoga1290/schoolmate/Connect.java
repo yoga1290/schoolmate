7f42530855d73aad34f97dce7f6a2f9b4301cf55
Youssef Gamil <yoga1290@gmail.com>
2013-06-29 03:04:34 +0200

    Connect.JSON instead of Connect.txt

--- src/yoga1290/schoolmate/Connect.java
+++ src/yoga1290/schoolmate/Connect.java
@@ -16,12 +16,11 @@ import android.os.Environment;
 
 public class Connect
 {
-	public static String KEY_STUDENTID="studentId";
 	public static boolean isConnected()
 	{
 		//TODO check aast id
 		try{
-			File file=new File(Environment.getExternalStorageDirectory().getPath()+"/connect.txt");
+			File file=new File(Environment.getExternalStorageDirectory().getPath()+"/connect.json");
 			return file.exists();
 		}catch(Exception e){return false;}
 	}
@@ -33,7 +32,7 @@ public class Connect
 	public static String OAuthFoursquareURI="https://foursquare.com/oauth2/authenticate?client_id=BZ4QPVWSF213QA2ICE1QSHIGDMCNZBW20QD3EXBVH0OHG3IT&response_type=code&redirect_uri=http://yoga1290.appspot.com/schoolmate/oauth/foursquare/callback/";
 	
 	// get AppAccessToken from https://graph.facebook.com/oauth/access_token?client_id=122683301250532&client_secret=***&grant_type=client_credentials
-	public static String OAuthFacebook_AppAccessToken="****";
+	public static String OAuthFacebook_AppAccessToken="*******";
 	
 	//Const:
 	public static String facebook="facebook";
@@ -43,10 +42,10 @@ public class Connect
 		try{
 			String res="";
 			JSONObject json=new JSONObject();
-			File file=new File(Environment.getExternalStorageDirectory().getPath()+"/connect.txt");
+			File file=new File(Environment.getExternalStorageDirectory().getPath()+"/connect.json");
 			if(file.exists())
 			{
-				FileInputStream in=new FileInputStream(Environment.getExternalStorageDirectory().getPath()+"/connect.txt");
+				FileInputStream in=new FileInputStream(Environment.getExternalStorageDirectory().getPath()+"/connect.json");
 				byte buff[]=new byte[200];
 				int o;
 				while((o=in.read(buff))>0)
@@ -60,7 +59,7 @@ public class Connect
 	}
 	public static void setData(JSONObject json) throws Exception
 	{
-		FileOutputStream out=new FileOutputStream(Environment.getExternalStorageDirectory().getPath()+"/connect.txt");
+		FileOutputStream out=new FileOutputStream(Environment.getExternalStorageDirectory().getPath()+"/connect.json");
 		out.write(json.toString().getBytes());
 		out.close();
 	}
@@ -71,6 +70,8 @@ public class Connect
 	
 }
 
+
+// Response Handler for "https://graph.facebook.com/me?access_token="; happens after ServerRequestHandler receives SERVICE?access_token=ACCESS_TOKEN
 class facebook implements URLThread_CallBack
 {
 	@Override
@@ -97,8 +98,44 @@ class facebook implements URLThread_CallBack
 			Connect.setData(res);
 			
 
-			//try updating user's data on the server
+			//try updating user's data on the server using Connect.json data
 				new URLThread("yoga1290.appspot.com/schoolmate/student?id="+res.getString("id")+"&pin="+res.getString("pin"), 
+						new URLThread_CallBack() {
+							@Override
+							public void URLCallBack(String response)
+							{
+								System.out.println("Updating user's data:\n"+response);
+							}
+						},
+						res.toString()).start();
+			
+			
+		}catch(Exception e){e.printStackTrace();}
+	}
+}
+
+
+class prifileCallback implements URLThread_CallBack
+{
+	@Override
+	public void URLCallBack(String response)
+	{
+		try{
+			JSONObject resp=new JSONObject(response);
+			JSONObject res=Connect.getData();
+			
+			Iterator<String> it=resp.keys();
+			String tmp;
+			while(it.hasNext())
+			{
+				tmp=it.next();
+					res.put(tmp, resp.get(tmp));
+			}
+			Connect.setData(res);
+			
+
+			//try updating user's data on the server using Connect.txt data
+				new URLThread("yoga1290.appspot.com/schoolmate/student?id="+res.getString("id")+"&pin=1"+res.getString("pin"), 
 						new URLThread_CallBack() {
 							@Override
 							public void URLCallBack(String response)
